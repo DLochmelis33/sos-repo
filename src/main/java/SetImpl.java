7bc999c4222fc4efcde4945ef84f227c8f6db48e
dlochmelis33 <dlochmelis@gmail.com>
2022-04-19 14:22:20 +0300

    Prepare Kotlin build

--- src/main/java/SetImpl.java
+++ src/main/java/SetImpl.java
@@ -1,28 +1,31 @@
 import java.util.Iterator;
 
 public class SetImpl<T extends Comparable<T>> implements Set<T> {
+
+    private final SetImplKt<T> delegate = new SetImplKt<>();
+
     @Override
     public boolean add(T value) {
-        return false;
+        return delegate.add(value);
     }
 
     @Override
     public boolean remove(T value) {
-        return false;
+        return delegate.remove(value);
     }
 
     @Override
     public boolean contains(T value) {
-        return false;
+        return delegate.contains(value);
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return delegate.isEmpty();
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return delegate.iterator();
     }
 }
---
+++ src/main/kotlin/SetImplKt.kt
@@ -0,0 +1,23 @@
+
+class SetImplKt<T : Comparable<T>> : Set<T> {
+
+    override fun add(value: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(value: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun contains(value: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isEmpty(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun iterator(): MutableIterator<T> {
+        TODO("Not yet implemented")
+    }
+}
